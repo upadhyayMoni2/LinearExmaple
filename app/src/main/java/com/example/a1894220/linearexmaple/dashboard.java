c9ca0468b21fbd9f75519c8bf07d32070e2f657f
1894220 <upadhyayMoni2>
2019-06-12 10:20:02 -0400

    changes for linearlayout

---
+++ app/src/main/java/com/example/a1894220/linearexmaple/dashboard.java
@@ -0,0 +1,13 @@
+package com.example.a1894220.linearexmaple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
